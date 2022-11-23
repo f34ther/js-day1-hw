fb37cab1d9a12991861f8adfc18039d8a70a44c7
Feather Lushenko <featherlushenko@gmail.com>
2022-11-23 11:31:08 -0600

    First commit

---
+++ main.js
@@ -0,0 +1,75 @@
+//match dog names
+let dog_string = 'Hello Max, my name is Dog, and I have purple eyes!'
+let dog_names = ['Max', 'HAS', "PuRple", 'dog']
+// console.log(dog_string)
+// console.log(dog_names)
+
+// function findWords() {
+//     let names = dog_names.map(name => {
+//         if (name = dog_string) {
+//             return 'Matched Dog_Name'
+//         } else {
+//             return 'No Matches'
+//         }
+//     })
+// }
+// console.log(findWords())
+// function findWords() {
+//     // console.log(dog_names.forEach(name => console.log(name)))
+//     // console.log(dog_string.forEach(words => console.log(words)))
+//     console.log(dog_string.split(' '))
+//     for (let name = dog_names.forEach(name){
+//         if (name in dog_string.split(' '));
+//         { return `Matched Dog_name ${name}` }
+//         else {
+//         return `No matches ${name}`
+//     }
+// }
+// )
+// }
+
+// console.log(findWords())
+
+// function findWords() {
+//     for (let name = dog_names.forEach(name => console.log(name))) {
+//         for (let word = dog_string.split(' ')) {
+//             if (name = word) {
+//                 return `Matched dog_name: ${name}`
+//             } else {
+//         return `No matches found: ${name}`}
+//     }
+// }
+
+// console.log(findWords())
+
+// function findWords() {
+//     for (let name = dog_names.forEach(name => console.log(name)));
+//     for (let word = dog_string.split(' ').forEach(item = console.log(item)));
+//     if (name = word) {
+//                 return `Matched dog_name: ${name}`
+//             } else {
+//                 return `No Matches: ${name}`
+//             }
+// }
+
+// console.log(findWords())
+
+
+function findWords(dog_names) {
+    for (let word = console.log(dog_string.split(' ')))
+        {
+            if (dog_names = word) 
+        {
+            return `Matched name: ${dog_names}`
+        } else
+    {
+        return `No matches: ${dog_names}`
+    }
+}
+}
+
+
+given_arr = ['Max', 'Baseball', 'Reboot', 'Goku', 'Trucks', 'Rodger']
+
+console.log(given_arr.splice(0, 1, 'even') && given_arr.splice(2, 1, 'even') && given_arr.splice(4, 1, 'even'))
+console.log(given_arr)
